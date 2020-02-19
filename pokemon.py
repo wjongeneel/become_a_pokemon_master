2ace63a137322689a3544e43a26af9e2c12ca7a0
wjongeneel <wjongeneel@users.noreply.github.com>
2020-02-19 22:51:58 +0100

    added method attack to class pokemon

--- pokemon.py
+++ pokemon.py
@@ -14,8 +14,7 @@ class Pokemon:
             self.knock_out()
             self.current_hp == 0
         else:
-            print("{name} has lost {amount} HP and now has {current_hp} HP"\
-            .format(name = self.name, amount = amount, current_hp = self.current_hp))
+            print("{name} has lost {amount} HP and now has {current_hp} HP".format(name = self.name, amount = amount, current_hp = self.current_hp))
 
 
     def add_health(self, amount):
@@ -25,8 +24,7 @@ class Pokemon:
             self.current_hp = self.max_hp
         else:
             self.current_hp += amount
-        print("{name} has gain {amount} HP and now has {current_hp} HP"\
-        .format(name = self.name, amount = amount, current_hp = self.current_hp))
+        print("{name} has gain {amount} HP and now has {current_hp} HP".format(name = self.name, amount = amount, current_hp = self.current_hp))
 
 
     def knock_out(self):
@@ -38,7 +36,34 @@ class Pokemon:
         self.is_knocked_out = False
         print("{name} has been revived".format(name = self.name))
 
+
+    def attack(self, pokemon):
+        base_damage = self.level
+        if self.type == "Fire" and pokemon.type == "Grass":
+            damage = base_damage * 2
+        elif self.type == "Fire" and pokemon.type == "Water":
+            damage = base_damage * 0.5
+        elif self.type == "Fire" and pokemon.type == "Fire":
+            damage = base_damage * 0.5
+        elif self.type == "Water" and pokemon.type == "Grass":
+            damage = base_damage * 0.5
+        elif self.type == "Water" and pokemon.type == "Water":
+            damage = base_damage * 0.5
+        elif self.type == "Water" and pokemon.type == "Fire":
+            damage = base_damage * 2
+        if self.type == "Grass" and pokemon.type == "Grass":
+            damage = base_damage * 0.5
+        elif self.type == "Grass" and pokemon.type == "Water":
+            damage = base_damage * 2
+        elif self.type == "Grass" and pokemon.type == "Fire":
+            damage = base_damage * 0.5
+        print("{name} attacks!".format(name = self.name))
+        pokemon.lose_health(damage)
+
+
+class Trainer:
+    def __init__(self, pokemon_list, name, number_of_potions, current_active_pokemon):
+
 charmander = Pokemon("Charmander", 100, "Fire", 100, 100, False)
-charmander.lose_health(30)
-charmander.add_health(50)
-#charmander.knock_out()
+bulbasaur = Pokemon("Bulbasaur", 100, "Grass", 100, 100, False)
+bulbasaur.attack(charmander)
